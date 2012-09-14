73909d91ff70606dfff523cef69a23cc6d1ce77f
Marius Gundersen <gundersen@gmail.com>
2012-09-14 22:08:37 +0200

    removed output file, added readme, fixed php script

--- index.php
+++ index.php
@@ -4,4 +4,10 @@ header('Content-Type: image/gif');
 ob_implicit_flush(true);
 ob_end_flush();
 
-passthru('pigs.bat');
+if(strtoupper(substr(PHP_OS, 0, 3)) === 'WIN'){
+	$cmd = "pigs.bat";
+}else{
+	$cmd = "./pigs.sh";
+}
+
+passthru($cmd);
